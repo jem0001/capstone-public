491d2cba5d6fb27441df96655d56e489d5f6b0c1
Jemuel <salcedojemuel25@gmail.com>
2024-11-21 09:09:55 +0800

    'frontend-origin'

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ const app = express();
 // MIDDLEWARES
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://sayap.vercel.app", credentials: true }));
 
 // MIDDLEWARES for security
 app.use(helmet());
