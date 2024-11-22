3cd5a7a0ea41e75fdd8c1f027b35ca2da345034c
Jemuel <salcedojemuel25@gmail.com>
2024-11-22 19:56:49 +0800

    'logout-clear-cookie'

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -209,7 +209,7 @@ const verifyEmail = async (req, res) => {
 };
 
 const logout = async (req, res) => {
-  res.clearCookie("token");
+  res.clearCookie("token", { secure: true, sameSite: "None" });
   res.status(200).json({ message: "Logout successful" });
 };
 
